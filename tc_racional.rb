5dad78809ef282d73a55ff9ea531bf5db68d9f7f
Ruben Guanche <alu0100536867@ull.edu.es>
2013-10-18 18:03:53 +0100

    pruebas unitarias

--- tc_racional.rb
+++ tc_racional.rb
@@ -4,5 +4,21 @@ require "./racional.rb"
 require "test/unit"
 
 class Test_Fraccion < Test::Unit::TestCase
+        def setup
+                @f1 = Fraccion.new(2,5)
+                @f2 = Fraccion.new(1,3)
+        end
+        def test_suma
+                assert_equal("11/15", @f1.suma(@f2.num,@f2.den))
+        end
+        def test_resta
+                assert_equal("1/15",@f1.resta(@f2.num,@f2.den))
+        end
+        def test_producto
+                assert_equal("2/15",@f1.producto(@f2.num,@f2.den))
+        end
+        def test_division
+                assert_equal("6/5",@f1.division(@f2.num,@f2.den))
+        end
 
 end
